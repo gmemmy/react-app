3df69bca595fa2278d5c0066554e15127aaf9907
Emmy <gmemmy22@gmail.com>
2019-06-11 17:14:05 +0100

    Initial commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,7 +8,7 @@ class Counter extends Component {
     return (
       <div>
         <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button>Increment</button>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
