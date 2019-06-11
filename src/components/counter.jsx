0cb7c11097172264ffd676109d0017b7b20fa525
Emmy <gmemmy22@gmail.com>
2019-06-11 19:05:33 +0100

    setup incremental logic for the counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { 
+  state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
+
+  //constructor() {
+    //super();
+   // this.handleIncrement = this.handleIncrement.bind(this);
+  //}
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 
+  getBadgeClasses() {
+    let classes = "badges m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    return count === 0 ? "Zero" : count;
   }
 }
- 
+
 export default Counter;
