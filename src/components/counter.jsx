dda65d88450ffe176fe5da75b6ee4781de74a99b
Emmy <gmemmy22@gmail.com>
2019-06-11 21:41:29 +0100

    add value and id props to counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,23 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ["tag1", "tag2", "tag3"]
   };
 
   //constructor() {
-    //super();
-   // this.handleIncrement = this.handleIncrement.bind(this);
+  //super();
+  // this.handleIncrement = this.handleIncrement.bind(this);
   //}
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  }
+    this.setState({ value: this.state.value + 1 });
+  };
 
   render() {
     return (
       <div>
+        <h4>{this.props.id}.</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
@@ -25,24 +26,24 @@ class Counter extends Component {
         >
           Increment
         </button>
-        <ul>
+        {/* <ul>
           {this.state.tags.map(tag => (
             <li key={tag}>{tag}</li>
           ))}
-        </ul>
+        </ul> */}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badges m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
